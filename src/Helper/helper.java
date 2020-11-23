3c652f4b48e281f947ed6595174a941a370c7cea
anhhn17 <74643541+anhhn17@users.noreply.github.com>
2020-11-23 13:40:50 +0700

    anhnh_qlnv

--- src/Helper/helper.java
+++ src/Helper/helper.java
@@ -13,17 +13,17 @@ import java.sql.DriverManager;
  * @author TRAN NAM
  */
 public class helper {
-    public static Connection ketnoi (String datablse){
-        try{
+     public static Connection ketnoi(String database){
+        try {
             String user="sa";
-            String pass ="123456789";
+            String pass="0338459391";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url="jdbc:sqlserver://DESKTOP-VHM9V13\\SQLEXPRESS:1433;databaseName="+datablse;
-            Connection cn= DriverManager.getConnection(url,user,pass);
+            String url="jdbc:sqlserver://DESKTOP-JQB11OG\\SQLEXPRESS:49677;databaseName="+database;
+            Connection cn= DriverManager.getConnection(url,user , pass);
             return cn;
-            }
-        catch(Exception a){
-            throw  new RuntimeException(a);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+        
     }
 }
